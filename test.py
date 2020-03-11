e9c10ef816e1d65781eaa2168889f8c30af36e51
Taaseen Ali <taaseen.ali89@gmail.com>
2020-03-11 17:11:34 -0400

    more tweaks

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from PIL import Image 
   
 # creating a image object 
-im = Image.open("crosswalk.jpg").convert('LA')
+im = Image.open("train.jpg")
 WIDTH = im.size[0]
 HEIGHT = im.size[1]
 px = im.load() 
@@ -12,7 +12,13 @@ def get_vector_from_dir(v1, v2):
 def initialize_id_vector(width, height):
     return [[-1 for i in range(width)] for i in range(height)]
 
-def id_bodies_from_img(image):
+def color_diff(vec1, vec2):
+    rdiff = abs(vec1[0] - vec2[0])
+    gdiff = abs(vec1[1] - vec2[1])
+    bdiff = abs(vec1[2] - vec2[2])
+    return rdiff + gdiff + bdiff
+
+def id_bodies_from_img(image, thrsh):
     WIDTH = image.size[0]
     HEIGHT = image.size[1]
     print(WIDTH)
@@ -22,7 +28,7 @@ def id_bodies_from_img(image):
 
     for k in range(HEIGHT):
         for i in range(WIDTH):
-            identify_pix(px, i, k, bodies, image.size)
+            identify_pix(px, i, k, bodies, image.size, thrsh)
     
 
     # for k in range(HEIGHT):
@@ -32,34 +38,34 @@ def id_bodies_from_img(image):
 
     for k in range(WIDTH):
         for i in range(HEIGHT):
-            px[k, i] = bodies[i][k], 255
+            px[k, i] = bodies[i][k], bodies[i][k], bodies[i][k] 
 
     im.show()
     return im
 
-def identify_pix(px, i, k, bodies, size):
-    threshold = 7
+def identify_pix(px, i, k, bodies, size, thrsh):
+    threshold = thrsh
     if (i == 0 and k == 0):
         bodies[k][i] = 0
         return
     if (i > 0):
-        if ((px[i, k][0] - px[i-1, k][0]) <= threshold):
+        if (color_diff(px[i, k], px[i-1, k]) <= threshold):
             bodies[k][i] = bodies[k][i-1]
             return 
         if (k > 0):
-            if ((px[i, k][0] - px[i-1, k-1][0]) <= threshold):
+            if (color_diff(px[i, k],  px[i-1, k-1]) <= threshold):
                 bodies[k][i] = bodies[k-1][i-1]
                 return
     if (k > 0):
-        if ((px[i, k][0] - px[i, k-1][0]) <= threshold):
+        if (color_diff(px[i, k], px[i, k-1]) <= threshold):
             bodies[k][i] = bodies[k-1][i]
             return
         if (i < size[0]-1):
-            if ((px[i, k][0] - px[i+1, k-1][0]) <= threshold):
+            if (color_diff(px[i, k], px[i+1, k-1]) <= threshold):
                 bodies[k][i] = bodies[k-1][i+1]
                 return
     
-    bodies[k][i] = px[i, k][0]
+    bodies[k][i] = int((float(color_diff(px[i, k], (255,255,255)))/255)*(threshold+50))
          
 
 # for i in range(WIDTH):
@@ -68,7 +74,7 @@ def identify_pix(px, i, k, bodies, size):
 #     print()
 
 
-id_bodies_from_img(id_bodies_from_img(im))
+id_bodies_from_img(id_bodies_from_img(id_bodies_from_img(im, 40), 40), 40)
 
 # print (px[4, 4]) 
 # px[4, 4] = (0, 0) 
